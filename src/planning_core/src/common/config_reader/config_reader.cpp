88722be91bd1335f7c4cfc95117e0e38d1587948
Fan Jiajiong <fanjiajion@126.com>
2025-01-07 00:20:58 +0800

    config_reader in process: vehicle

--- src/planning_core/src/common/config_reader/config_reader.cpp
+++ src/planning_core/src/common/config_reader/config_reader.cpp
@@ -3,6 +3,46 @@
 namespace Planning {
   ConfigReader::ConfigReader()
   {
+    // get the path of workspace/install/planning_core/share/planning_core
+    std_string configFilePath =
+        ament_index_cpp::get_package_share_directory("planning_core");
+
+    // get config file
+    planningConfig = YAML::LoadFile(configFilePath + "/config/planning_static_obs_config.yaml");
+  
+  }
+
+  void ConfigReader::readVehicleConfig(VehicleStruct &vehicle, const std_string &vehicleName)
+  {
+    vehicle.id_ = planningConfig["vehicle"][vehicleName]["id"].as<uint8>();
+    vehicle.frame_ = planningConfig["vehicle"][vehicleName]["frame"].as<std_string>();
+    vehicle.length_ = planningConfig["vehicle"][vehicleName]["length"].as<float32>();
+    vehicle.width_ = planningConfig["vehicle"][vehicleName]["width"].as<float32>();
+    vehicle.pose_x_ = planningConfig["vehicle"][vehicleName]["pose_x"].as<float32>();
+    vehicle.pose_y_ = planningConfig["vehicle"][vehicleName]["pose_y"].as<float32>();
+    vehicle.pose_theta_ = planningConfig["vehicle"][vehicleName]["pose_theta"].as<float32>();
+    vehicle.speed_init_ = planningConfig["vehicle"][vehicleName]["speed_init"].as<float32>();
+
+    if (vehicleName != "ego_car")
+    {
+      VehiclePairs.emplace(vehicle.id_, vehicle);
+    }
+  }
+
+    void ConfigReader::readVehicleConfig()
+  {
+    try
+    {
+      readPNCMapConfig();
+      readVehicleConfig(egoCar, "ego_car");
+      readVehicleConfig(tpCar1, "obs_car1");
+      readVehicleConfig(tpCar2, "obs_car2");
+      readVehicleConfig(tpCar3, "obs_car3");
+    }
+    catch(const std::exception& e)
+    {
+      RCLCPP_ERROR(rclcpp::get_logger("config"), "Failed to load VehicleConfig: %s", e.what());
+    }
   }
 
 } // namespace Planning
--- src/planning_core/src/common/config_reader/config_reader.h
+++ src/planning_core/src/common/config_reader/config_reader.h
@@ -10,95 +10,151 @@
 namespace Planning {
   struct VehicleStruct
   {
-    uint8           id_{ 0U };
-    std_string      frame_{ "" };
-    float32         length_{ 0.0F };    // uint: m
-    float32         width_{ 0.0F };     // uint: m
-    float32         pose_x_{ 0.0F };    // uint: m
-    float32         pose_y_{ 0.0F };    // uint: m
-    float32         pose_theta_{ 0.0F };
-    float32         speed_init_{ 0.0F };
+    uint8 id_{ 0U };
+    std_string frame_{ "" };
+    float32 length_{ 0.0F }; // uint: m
+    float32 width_{ 0.0F };  // uint: m
+    float32 pose_x_{ 0.0F }; // uint: m
+    float32 pose_y_{ 0.0F }; // uint: m
+    float32 pose_theta_{ 0.0F };
+    float32 speed_init_{ 0.0F };
   };
 
   struct PNCMapStruct
   {
-    std_string      frame_{ "" };
-    uint8           type_{ 0U };
-    float32         road_length{ 0.0F };    // uint: m
-    float32         road_half_width{ 0.0F };  // uint: m
-    float32         segment_len{ 0.0F };    // uint: m
-    float32         speed_limit{ 0.0F };    // uint: m/s
+    std_string frame_{ "" };
+    uint8 type_{ 0U };
+    float32 road_length{ 0.0F };     // uint: m
+    float32 road_half_width{ 0.0F }; // uint: m
+    float32 segment_len{ 0.0F };     // uint: m
+    float32 speed_limit{ 0.0F };     // uint: m/s
   };
 
   struct ReferenceLineStruct
   {
-    uint8           type_{ 0U };
-    uint32          front_size_{ 0U };
-    uint32          back_size_{ 0U };
+    uint8 type_{ 0U };
+    uint32 front_size_{ 0U };
+    uint32 back_size_{ 0U };
   };
 
   struct GlobalPathStruct
   {
-    uint8           type_{ 0U };
+    uint8 type_{ 0U };
   };
 
   struct LocalPathStruct
   {
-    uint8           curve_type_{ 0U };
-    uint32          path_size_{ 0U };
+    uint8 curve_type_{ 0U };
+    uint32 path_size_{ 0U };
   };
 
   struct LocalSpeedsStruct
   {
-    uint32          speeds_size_{ 0U };
+    uint32 speeds_size_{ 0U };
   };
 
   struct DecisionStruct
   {
-    float32        safe_dis_lat_{ 0.0F };    // uint: m
-    float32        safe_dis_lon_{ 0.0F };    // uint: m
+    float32 safe_dis_lat_{ 0.0F }; // uint: m
+    float32 safe_dis_lon_{ 0.0F }; // uint: m
   };
 
   struct ProcessStruct
   {
-    float32       obs_dis_{ 0.0F };    // uint: s
+    float32 obs_dis_{ 0.0F }; // uint: s
   };
 
   class ConfigReader
   {
   public:
     ConfigReader();
+    ConfigReader(const ConfigReader &) = delete;
+    ConfigReader &operator=(const ConfigReader &) = delete;
     ~ConfigReader() = default;
 
-  private:
     // vehicle
-    static VehicleStruct           egoCar;
-    static VehicleStruct           tpCar1;
-    static VehicleStruct           tpCar2;
-    static VehicleStruct           tpCar3;
+    void readVehicleConfig();
 
-    static std::unordered_map<uint8, VehicleStruct> VehiclePairs;
+    void readVehicleConfig(VehicleStruct &vehicle, const std_string &vehicleName);
+
+    inline VehicleStruct getEgoCar() const { return egoCar; }
+
+    inline VehicleStruct getTpCar1() const { return tpCar1; }
+
+    inline VehicleStruct getTpCar2() const { return tpCar2; }
+
+    inline VehicleStruct getTpCar3() const { return tpCar3; }
+
+    inline std::unordered_map<uint8, VehicleStruct> getVehiclePairs() const { return VehiclePairs; }
 
     // pnc_map
-    static PNCMapStruct            pncMap;
+    void readPNCMapConfig();
 
-    // reference_line
-    static ReferenceLineStruct     referenceLine;
+    inline PNCMapStruct getPNCMap() const { return pncMap; }
 
-    // global_path
-    static GlobalPathStruct        globalPath;
+    // global path planning
+    void readGlobalPathConfig();
 
-    // local_path
-    static LocalPathStruct         localPath;
+    inline GlobalPathStruct getGlobalPath() const { return globalPath; }
 
-    // local_speeds
-    static LocalSpeedsStruct       localSpeeds;
+    // reference line
+    void readReferenceLineConfig();
+
+    inline ReferenceLineStruct getReferenceLine() const { return referenceLine; }
+
+    // local path planning
+    void readLocalPathConfig();
+
+    inline LocalPathStruct getLocalPath() const { return localPath; }
+
+    // local speeds planning
+    void readLocalSpeedsConfig();
+
+    inline LocalSpeedsStruct getLocalSpeeds() const { return localSpeeds; }
 
     // decision
-    static DecisionStruct          decision;
+    void readDecisionConfig();
+
+    inline DecisionStruct getDecision() const { return decision; }
 
     // process
-    static ProcessStruct           process;
+    void readProcessConfig();
+
+    inline ProcessStruct getProcess() const { return process; }
+
+    // move_cmd
+    void readMoveCmdConfig();
+
+  private:
+    YAML::Node planningConfig;
+    // vehicle
+    VehicleStruct egoCar;
+    VehicleStruct tpCar1;
+    VehicleStruct tpCar2;
+    VehicleStruct tpCar3;
+
+    std::unordered_map<uint8, VehicleStruct> VehiclePairs;
+
+    // pnc_map
+    PNCMapStruct pncMap;
+
+    // reference_line
+    ReferenceLineStruct referenceLine;
+
+    // global_path
+    GlobalPathStruct globalPath;
+
+    // local_path
+    LocalPathStruct localPath;
+
+    // local_speeds
+    LocalSpeedsStruct localSpeeds;
+
+    // decision
+    DecisionStruct decision;
+
+    // process
+    ProcessStruct process;
   };
 } // namespace Planning
 #endif // !CONFIG_READER_H_
