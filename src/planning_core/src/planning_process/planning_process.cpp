ead8bb4ad0496078d99ddf37141b745636dc9fa4
Fanjj1994 <fanjiajion@126.com>
2025-03-12 23:42:24 +0800

    create map client &global path client.

--- src/planning_core/src/planning_process/planning_process.cpp
+++ src/planning_core/src/planning_process/planning_process.cpp
@@ -10,10 +10,78 @@ namespace Planning {
     // Read config file for planning process
     configReaderProcess = std::make_unique<ConfigReader>();
     configReaderProcess->readProcessConfig();
-    auto obsDis = configReaderProcess->getProcess().obs_dis_;
+    obsDis = configReaderProcess->getProcess().obs_dis_;
+
+    // create PNCMap & Global Path Client
+    pncMapClient = this->create_client<base_msgs::srv::PNCMapService>("pnc_map_server");
+    globalPathClient = this->create_client<base_msgs::srv::GlobalPathService>("global_path_server");
   }
 
   boolean PlanningProcess::process()
+  {
+    // set 1 second timer to wait rviz2 & xacro to be ready
+    rclcpp::Rate rate(1s);
+    rate.sleep();
+
+    if (initPlanning() == false)
+    {
+      RCLCPP_ERROR(this->get_logger(), "planning init failed!");
+      return false;
+    }
+    
+    // planning main process
+
+    return true;
+  }
+
+  boolean PlanningProcess::initPlanning()
+  {
+    boolean initPlanningResult = true;
+    // TODO: create Tp
+
+    // connect map server
+    if (connectServer(pncMapClient) == false)
+    {
+      RCLCPP_ERROR(this->get_logger(), "connect pnc map server failed!");
+      initPlanningResult = false;
+    } 
+
+    // get map
+    if (requestPNCMap() == false)
+    {
+      RCLCPP_ERROR(this->get_logger(), "request pnc map failed!");
+      initPlanningResult = false;
+    }
+
+    // connect global path server
+    if (connectServer(globalPathClient) == false)
+    {
+      RCLCPP_ERROR(this->get_logger(), "connect global path server failed!");
+      initPlanningResult = false;
+    }
+
+    // get global path
+    if (requestGlobalPath() == false)
+    {
+      RCLCPP_ERROR(this->get_logger(), "request global path failed!");
+      initPlanningResult = false;
+    }
+    
+    return initPlanningResult;
+  }
+
+  template <typename T>
+  boolean PlanningProcess::connectServer(const T &client)
+  {
+    return true;
+  }
+
+  boolean PlanningProcess::requestPNCMap()
+  {
+    return true;
+  }
+
+  boolean PlanningProcess::requestGlobalPath()
   {
     return true;
   }
--- src/planning_core/src/planning_process/planning_process.h
+++ src/planning_core/src/planning_process/planning_process.h
@@ -2,6 +2,10 @@
 #define PLANNING_PROCESS_H_
 
 #include "rclcpp/rclcpp.hpp"
+#include "base_msgs/msg/pnc_map.hpp"
+#include "base_msgs/srv/pnc_map_service.hpp"
+#include "base_msgs/srv/global_path_service.hpp"
+#include "nav_msgs/msg/path.hpp"
 #include "common_type.h"
 #include "config_reader.h"
 #include "ego_car_base.h"
@@ -19,6 +23,8 @@
 namespace Planning {
   static constexpr uint8 PROCESS_FAILURE{ 255U };
 
+  using namespace::std::chrono_literals;
+
   class PlanningProcess : public rclcpp::Node
   {
   public:
@@ -26,8 +32,33 @@ namespace Planning {
     ~PlanningProcess() = default;
     boolean process();
 
+    inline base_msgs::msg::PNCMap getPNCMap() const
+    {
+      return pncMap;
+    }
+
+    inline nav_msgs::msg::Path getGlobalPath() const
+    {
+      return globalPath;
+    }
+
   private:
     std::unique_ptr<ConfigReader> configReaderProcess;
+    float64 obsDis; // obstacle distance
+    base_msgs::msg::PNCMap pncMap;
+    nav_msgs::msg::Path globalPath;
+    // map client
+    rclcpp::Client<base_msgs::srv::PNCMapService>::SharedPtr pncMapClient;
+    // global path client
+    rclcpp::Client<base_msgs::srv::GlobalPathService>::SharedPtr globalPathClient;
+
+    boolean initPlanning();
+    
+    template <typename T>
+    boolean connectServer(const T &client); // pnc map or global path client
+
+    boolean requestPNCMap();
+    boolean requestGlobalPath();
   };
 } // namespace Planning
 #endif // !PLANNING_PROCESS_H_
