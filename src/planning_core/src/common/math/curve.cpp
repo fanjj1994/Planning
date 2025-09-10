261f7199d843b5a513f9f397924f50d89f71a35f
fan jiajiong <fanjiajion@126.com>
2025-09-10 23:40:35 +0800

    [ReferenceLine] 1. Write ReferenceLineCreator for 1st edition; 2. Write findMatchPointIndex, calculateProjectedPointParameters in math/curve lib; 3. format planning_static_obs_config.yaml.

--- src/planning_core/src/common/math/curve.cpp
+++ src/planning_core/src/common/math/curve.cpp
@@ -1,5 +1,153 @@
 #include "curve.h"
 
-namespace Planning {
+namespace Planning
+{
+int16 Curve::findMatchPointIndex(const nav_msgs::msg::Path& path, const int16 lastMatchPointIndex,
+                                 const geometry_msgs::msg::PoseStamped& targetPoint)
+{
+  const int16 pathSize = path.poses.size();
+  if (pathSize <= 1)
+  {
+    return pathSize - 1;
+  }
+  float64 distanceMin = std::numeric_limits<float64>::max();
+  int16 closestIndex = -1;
 
-} // namespace Planning
+  for (int16 i = 0; i < pathSize; i++)
+  {
+    float64 distance = std::hypot(path.poses[i].pose.position.x - targetPoint.pose.position.x,
+                                  path.poses[i].pose.position.y - targetPoint.pose.position.y);
+    if (distance < distanceMin)
+    {
+      if (abs(lastMatchPointIndex - i) > MAX_JUMP)  // prevent jumping
+      {
+        continue;
+      }
+      distanceMin = distance;
+      closestIndex = i;
+    }
+  }
+  return closestIndex;
+}
+
+void Curve::calculateProjectedPointParameters(base_msgs::msg::Referline& referenceline)
+{
+  const uint16 pathSize = referenceline.refer_line.size();
+  if (pathSize < 3U)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("math"),
+                 "Referline size is less than 3, cannot calculate projected point parameters!");
+    return;
+  }
+
+  // calculate rs
+  float64 rs = 0.0;
+  for (uint16 i = 0U; i < pathSize; i++)
+  {
+    if (i == 0U)
+    {
+      rs = 0.0;
+    }
+    else
+    {
+      // std::hypot is similar with sqrt(x^2 + y^2), which calculates two points' distance safely without overflow
+      rs += std::hypot(
+          referenceline.refer_line[i].pose.pose.position.y - referenceline.refer_line[i - 1U].pose.pose.position.y,
+          referenceline.refer_line[i].pose.pose.position.x - referenceline.refer_line[i - 1U].pose.pose.position.x);
+    }
+    referenceline.refer_line[i].rs = rs;
+  }
+
+  // calculate heading
+  for (uint16 i = 0U; i < pathSize; i++)
+  {
+    if (i < pathSize - 1U)  // not the last point
+    {
+      referenceline.refer_line[i].rtheta = std::atan2(
+          referenceline.refer_line[i + 1U].pose.pose.position.y - referenceline.refer_line[i].pose.pose.position.y,
+          referenceline.refer_line[i + 1U].pose.pose.position.x - referenceline.refer_line[i].pose.pose.position.x);
+    }
+    else  // the last point
+    {
+      referenceline.refer_line[i].rtheta = std::atan2(
+          referenceline.refer_line[i].pose.pose.position.y - referenceline.refer_line[i - 1U].pose.pose.position.y,
+          referenceline.refer_line[i].pose.pose.position.x - referenceline.refer_line[i - 1U].pose.pose.position.x);
+    }
+  }
+
+  // calculate kappa
+  for (uint16 i = 0U; i < pathSize; i++)
+  {
+    if (i < pathSize - 1U)  // not the last point
+    {
+      const float64 distance = std::hypot(
+          referenceline.refer_line[i + 1U].pose.pose.position.y - referenceline.refer_line[i].pose.pose.position.y,
+          referenceline.refer_line[i + 1U].pose.pose.position.x - referenceline.refer_line[i].pose.pose.position.x);
+      if (distance <= EPSILON)
+      {
+        referenceline.refer_line[i].rkappa = 0.0;  // prevent division by zero
+      }
+      else
+      {
+        // use kappa's definition: kappa = dtheta / ds
+        referenceline.refer_line[i].rkappa =
+            (referenceline.refer_line[i + 1U].rtheta - referenceline.refer_line[i].rtheta) / distance;
+      }
+    }
+    else  // the last point
+    {
+      const float64 distance = std::hypot(
+          referenceline.refer_line[i].pose.pose.position.y - referenceline.refer_line[i - 1U].pose.pose.position.y,
+          referenceline.refer_line[i].pose.pose.position.x - referenceline.refer_line[i - 1U].pose.pose.position.x);
+      if (distance <= EPSILON)
+      {
+        referenceline.refer_line[i].rkappa = 0.0;  // prevent division by zero
+      }
+      else
+      {
+        // use kappa's definition: kappa = dtheta / ds
+        referenceline.refer_line[i].rkappa =
+            (referenceline.refer_line[i].rtheta - referenceline.refer_line[i - 1U].rtheta) / distance;
+      }
+    }
+  }
+
+  // calculate dkappa
+  for (uint16 i = 0U; i < pathSize; i++)
+  {
+    if (i < pathSize - 1U)  // not the last point
+    {
+      const float64 distance = std::hypot(
+          referenceline.refer_line[i + 1U].pose.pose.position.y - referenceline.refer_line[i].pose.pose.position.y,
+          referenceline.refer_line[i + 1U].pose.pose.position.x - referenceline.refer_line[i].pose.pose.position.x);
+      if (distance <= EPSILON)
+      {
+        referenceline.refer_line[i].rdkappa = 0.0;  // prevent division by zero
+      }
+      else
+      {
+        // use dkappa's definition: dkappa / ds
+        referenceline.refer_line[i].rdkappa =
+            (referenceline.refer_line[i + 1U].rkappa - referenceline.refer_line[i].rkappa) / distance;
+      }
+    }
+    else  // the last point
+    {
+      const float64 distance = std::hypot(
+          referenceline.refer_line[i].pose.pose.position.y - referenceline.refer_line[i - 1U].pose.pose.position.y,
+          referenceline.refer_line[i].pose.pose.position.x - referenceline.refer_line[i - 1U].pose.pose.position.x);
+      if (distance <= EPSILON)
+      {
+        referenceline.refer_line[i].rdkappa = 0.0;  // prevent division by zero
+      }
+      else
+      {
+        // use dkappa's definition: dkappa / ds
+        referenceline.refer_line[i].rdkappa =
+            (referenceline.refer_line[i].rkappa - referenceline.refer_line[i - 1U].rkappa) / distance;
+      }
+    }
+  }
+}
+
+}  // namespace Planning
--- src/planning_core/src/common/math/curve.h
+++ src/planning_core/src/common/math/curve.h
@@ -3,15 +3,29 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include <cmath>
+#include "common_type.h"
+#include "base_msgs/msg/local_path.hpp"
+#include "base_msgs/msg/referline.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 
-namespace Planning {
-  class Curve
-  {
-  public:
-    Curve() = default;
-    ~Curve() = default;
+namespace Planning
+{
+class Curve
+{
+private:
+  static constexpr uint8 MAX_JUMP = 100U;     // max jump between two adjacent matchpoint index
+  static constexpr float64 EPSILON = 1.0e-6;  // a small value
 
-  private:
-  };
-} // namespace Planning
-#endif // CURVE_H_
+public:
+  Curve() = default;
+  ~Curve() = default;
+
+  // find matchpoint index
+  static int16 findMatchPointIndex(const nav_msgs::msg::Path& path, const int16 lastMatchPointIndex,
+                                   const geometry_msgs::msg::PoseStamped& targetPoint);
+  // find projected point's parameters
+  static void calculateProjectedPointParameters(base_msgs::msg::Referline& referenceline);
+};
+}  // namespace Planning
+#endif  // CURVE_H_
