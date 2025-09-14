c3ee4fef99c2437a92aaf72f05d965aed32a7939
fan jiajiong <fanjiajion@126.com>
2025-09-14 23:33:17 +0800

    [ReferenceLine] 1. Fulfill reference line creation implementation in planning_process part.

--- src/planning_core/src/planning_process/planning_process.h
+++ src/planning_core/src/planning_process/planning_process.h
@@ -6,6 +6,12 @@
 #include "base_msgs/srv/pnc_map_service.hpp"
 #include "base_msgs/srv/global_path_service.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2/LinearMath/Quaternion.h"
+
 #include "common_type.h"
 #include "config_reader.h"
 #include "ego_car_base.h"
@@ -20,39 +26,67 @@
 #include <cmath>
 #include <algorithm>
 
-namespace Planning {
-  static constexpr uint8 PROCESS_FAILURE{ 255U };
+namespace Planning
+{
+static constexpr uint8 PROCESS_FAILURE{ 255U };
 
-  using namespace ::std::chrono_literals; // for 1s block
+using namespace ::std::chrono_literals;  // for 1s block
 
-  class PlanningProcess : public rclcpp::Node
+class PlanningProcess : public rclcpp::Node
+{
+public:
+  PlanningProcess();
+  ~PlanningProcess() = default;
+  boolean process();
+
+  inline base_msgs::msg::PNCMap getPNCMap() const
   {
-  public:
-    PlanningProcess();
-    ~PlanningProcess() = default;
-    boolean process();
+    return pncMap;
+  }
 
-    inline base_msgs::msg::PNCMap getPNCMap() const { return pncMap; }
+  inline nav_msgs::msg::Path getGlobalPath() const
+  {
+    return globalPath;
+  }
 
-    inline nav_msgs::msg::Path getGlobalPath() const { return globalPath; }
+private:
+  std::unique_ptr<ConfigReader> configReaderProcess;
+  std::shared_ptr<VehicleInfoBase> egoCar;  // ego car
+  float64 obsDis;                           // obstacle distance
 
-  private:
-    std::unique_ptr<ConfigReader> configReaderProcess;
-    float64 obsDis; // obstacle distance
-    base_msgs::msg::PNCMap pncMap;
-    nav_msgs::msg::Path globalPath;
-    // map client
-    rclcpp::Client<base_msgs::srv::PNCMapService>::SharedPtr pncMapClient;
-    // global path client
-    rclcpp::Client<base_msgs::srv::GlobalPathService>::SharedPtr globalPathClient;
+  // tf broadcaster: broadcast vehicle's initial information (e.g., pose info) to control module
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tfBroadcaster;
+  // tf listener: listen to the transform between map frame and vehicle frame from control module
+  std::unique_ptr<tf2_ros::Buffer> tfBuffer;
+  std::shared_ptr<tf2_ros::TransformListener> tfListener;
 
-    boolean initPlanning();
+  base_msgs::msg::PNCMap pncMap;
+  nav_msgs::msg::Path globalPath;
+  // map client
+  rclcpp::Client<base_msgs::srv::PNCMapService>::SharedPtr pncMapClient;
+  // global path client
+  rclcpp::Client<base_msgs::srv::GlobalPathService>::SharedPtr globalPathClient;
 
-    template <typename T>
-    boolean connectServer(const T &client); // pnc map or global path client
+  std::shared_ptr<ReferenceLineCreator> referenceLineCreator;                    // reference line creator
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr referenceLineRvizPublisher;  // reference line rviz publisher
+  rclcpp::TimerBase::SharedPtr runtime;                                          // runtime for planning process module
 
-    boolean requestPNCMap();
-    boolean requestGlobalPath();
-  };
-} // namespace Planning
-#endif // !PLANNING_PROCESS_H_
+  boolean initPlanning();
+
+  //// spawn vehicle, init and broadcast vehicle's pose
+  void spawnVehicle(const std::shared_ptr<VehicleInfoBase>& vehicle);
+
+  // listen to vehicle's real-time pose from control module
+  void getVehicleLocation(const std::shared_ptr<VehicleInfoBase>& vehicle);
+
+  // callback function for planning process
+  void planningCallback();
+
+  template <typename T>
+  boolean connectServer(const T& client);  // pnc map or global path client
+
+  boolean requestPNCMap();
+  boolean requestGlobalPath();
+};
+}  // namespace Planning
+#endif  // !PLANNING_PROCESS_H_
