f33f05017ae68e27827186a55d99fc4408c5b9fa
fanjj1994 <fanjiajion@126.com>
2026-03-06 00:36:36 +0800

    Decision: implement decision part. 1. draft header file.

--- src/planning_core/src/decision_center/decision_center.h
+++ src/planning_core/src/decision_center/decision_center.h
@@ -5,9 +5,18 @@
 #include "config_reader.h"
 #include "ego_car_base.h"
 #include "tp_base.h"
+#include <vector>
 
 namespace Planning
 {
+  enum class SLPointType : uint8 // decision point type
+  {
+    DECISION_LEFT_PASS = 0U,   // deicide to pass from left side
+    DECISION_RIGHT_PASS = 1U,  // decide to pass from right side
+    DEISION_STOP = 2U,         // decide to stop the car at this point
+    DEISION_START = 3U,        // start point of the decision
+    DECISION_END = 4U          // end point of the decision
+  };
   class DecisionCenter
   {
   public:
